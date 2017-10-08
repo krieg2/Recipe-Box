d2647f1c332d46ae50ae3706a75e052670975faa
krieg2 <aaronk1@yahoo.com>
2017-10-08 00:51:34 -0400

    adding product carousels

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,11 +17,7 @@ var recipesTitles = [];
 var recipeImg=[];
 var recipeID=[];
 var baseURI;
-var ingredientsList=[];
-var ingredientNames=[];
 var timerId;
-
-
 
 
 function ajax(URL, APIkey, CALLBACK){ //ajax function for search recipes 
@@ -47,7 +43,7 @@ function searchRecipesCallback(response){ //this is the callback function for th
 
 	// going to get the title from each result and console log it 
 	for(i=0; i<response.results.length; i++){
-		//console.log(response.results[i].title);
+		////console.log(response.results[i].title);
 		recipesTitles.push(response.results[i].title);
 		recipeImg.push(response.results[i].image);
 		recipeID.push(response.results[i].id);	
@@ -69,18 +65,18 @@ function submitSearch(event){ //this is the function for the submit button on th
 	if(!cuisine == "" && $('input[name=type]:checked').length > 0){
 		selectedRadioButton = $('input[name=type]:checked').val();
 		searchQueryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?query="+ SearchQueryParameter +"&cuisine=" + cuisine +"&type="+ selectedRadioButton;
-		console.log(searchQueryURL);
+		//console.log(searchQueryURL);
 	}
 	// if just the cuisine filter is filled out 
 	else if(!cuisine == ""){
 		searchQueryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?query="+ SearchQueryParameter +"&cuisine=" + cuisine;
-		console.log(searchQueryURL);
+		//console.log(searchQueryURL);
 	}
 	// if just the checkbox filter is selected 
 	else if($('input[name=type]:checked').length > 0){
 		selectedRadioButton = $('input[name=type]:checked').val();
 		searchQueryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?query="+ SearchQueryParameter +"&type=" + selectedRadioButton;
-		console.log(searchQueryURL);
+		//console.log(searchQueryURL);
 	}
 
 	// if no filters are selected 
@@ -134,49 +130,48 @@ $("#submit").on("click", submitSearch);
 }*/
 
 // Walmart API lookup by UPC.
-function productLookup(upc){
-
-	//event.preventDefault();
-	//var searchUPC = $("#").val().trim();
-
+function productLookup(upc, ingredientNum){
+                         
 	var searchQueryURL = "https://cors-anywhere.herokuapp.com/" + 
-						 "http://api.walmartlabs.com/v1/items?apiKey=" +
+	                     "http://api.walmartlabs.com/v1/items?apiKey=" +
 	                     "z5m92qf29tv7u76f4vaztra4" +
 	                     "&upc=" + upc;
+
+      /*dataType: "json",
+      indexValue: {upc: upc, ingredientNum: ingredientNum},
+      jsonpCallback: "productLookupCallback",
+      headers: {	
+       	"Content-Type": "application/jsonp",
+       	"X-Requested-With": "XMLHttpRequest"
+	  }*/
 
 	$.ajax({
       url: searchQueryURL,
       method: "GET",
-      dataType: "json",
       headers: {	
-      	"Content-Type": "application/json",
-      	"Origin": "http://localhost:8080"
-		}
+       	"X-Requested-With": "XMLHttpRequest"
+	  }
     }).done(function(response){
 
-        console.log("UPC response");
-    	console.log(response);
+        //console.log("UPC response "+upc);
+    	//console.log(response);
 
         if(typeof(response.items) !== 'undefined'){
-        	if(typeof(response.items[0].salePrice) !== 'undefined'){
-		    	var price = response.items[0].salePrice;
+        	//if(typeof(response.items[0].salePrice) !== 'undefined'){
+
+		    	//var price = response.items[0].salePrice;
 		    	var name = response.items[0].name;
 		    	var imgUrl = response.items[0].thumbnailImage;
 
-		    	var imgTag = $("<img>");
-		    	var imgDiv = $("<div>");
-		    	imgDiv.text(name + " : " + price);
-		    	imgTag.attr("src", imgUrl);
-		    	imgDiv.append(imgTag);
-		    	$("#shopping-cart").append(imgDiv);
-	    	}
+		    	addItemToCarousel(ingredientNum, name, imgUrl);
+	    	//}
     	}
 
     });
 }
 
-// Get products for ingredient.
-function ingredientsToProduct(){
+// Get products and UPC codes for each ingredient.
+function ingredientsToProduct(ingredientNames){
 
     $.ajax({
       url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/map",
@@ -188,34 +183,38 @@ function ingredientsToProduct(){
 		},
 	  data: JSON.stringify({
 	  	"ingredients": ingredientNames,
-	  	"servings": 2
+	  	"servings": 1
 	  }),
 	  processData: false,
 	  dataType: "json"
     }).done(function(response) {
 
-        console.log("ingredients to product results");
-    	console.log(response);
+        //console.log("ingredients to product results");
+    	//console.log(response);
     	var limit = 5;
-    	var x = 0;
+    	var delay = 0;
     	for(var i=0; i<response.length; i++){
-    		
-    		for(var j=0; j<limit; j++){
+    		var n = response[i].products.length;
+
+    		for(var j=0; j<n && j<limit; j++){
     			if(typeof(response[i].products[j]) !== 'undefined'){
+
 	    			var upc = response[i].products[j].upc;
-	    			x += 2000;
-	    			console.log("looking up: " + upc);
-	    			timerId = setTimeout(productLookup(upc), x);
+
+	    			delay += 500;
+
+	    			timerId = setTimeout(productLookup, delay, upc, i);
     			}
     		}
+    		$("#ingredient_"+i).append(createCarousel(i));
     	}
-    	$("#shopping-panel").removeClass("hidden");
 
     });
 }
 
 //Get recipe information.
 function recipeIngredients(event){
+
     var s = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" +
          "583672" + "/information";
 	$.ajax({
@@ -226,42 +225,105 @@ function recipeIngredients(event){
 		"Accept": "application/json"
 		}
     }).done(function(response) {
-    	console.log(response)
+    	//console.log(response)
     });
 }
 
 $("#recipe-images").on("click","img",function(event){
+
 	event.preventDefault();
 	var recipeID = $(this).attr("data-recipe-id");
-	//console.log("hi");
+
 	$.ajax({
       url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk?ids="+recipeID+"&includeNutrition=false",
       method: "GET",
       headers: {	
       	"X-Mashape-Key": apiKey,
       	"Content-Type": "application/json",
-		},
+		}
     }).done(function(response) {
-    	console.log(response);
-    	for(var i=0;i<response[0].extendedIngredients.length;i++){
+
+    	//console.log(response); 	
+    	
+    	//Store the ingredients in array.
+    	var ingredientsList=[];
+    	var ingredientNames=[];
+    	for(var i=0; i < response[0].extendedIngredients.length; i++){
+
     		ingredientsList.push(response[0].extendedIngredients[i].originalString);
-            ingredientNames.push(response[0].extendedIngredients[i].name);
+    		ingredientNames.push(response[0].extendedIngredients[i].name);
     	}
-    	createIngredientList();
-    	ingredientsToProduct();
+
+    	//Add the ingredients to the page.
+    	createIngredientList(ingredientsList);
+
+    	//Lookup the UPCs for each ingredient.
+    	ingredientsToProduct(ingredientNames);
+
+    	//Add the recipe to the page.
+		var recipe = response[0].instructions;
+    	recipeDiv = $("<div>");
+		recipeDiv.html("<br><h3><strong>Recipe: </h3></strong><br><p>"+recipe+"</p>");
+		$("#ingredients").append(recipeDiv);   
     });
 })
 
-
-
-function createIngredientList(){
+function createIngredientList(ingredientsList){
 	
-	for(var i=0;i<ingredientsList.length;i++){
+	for(var i=0; i < ingredientsList.length; i++){
+
 		ingredientDiv = $("<div>");
 		ingredientDiv.html(ingredientsList[i]);
+		ingredientDiv.attr("id", "ingredient_"+i);
+
+		ingredientDiv.css("min-height", "120px");
 		$("#ingredients").append(ingredientDiv);
 	}
 	$("#recipe-panel").addClass("hidden");
 	$("#ingredient-panel").removeClass("hidden");
 
+}
+
+
+function createCarousel(i){
+
+    var id = "carousel_" + i;
+	var outerDiv = $("<div class='carousel slide' data-ride='carousel'>")
+    outerDiv.attr("id", id);
+
+    var oList = $("<ol class='carousel-indicators'>");
+    outerDiv.append(oList);
+
+    var innerDiv = $("<div class='carousel-inner'>");
+    outerDiv.append(innerDiv);
+
+    outerDiv.append("<a class='left carousel-control' href='#carousel_" + i + 
+    	"' role='button' data-slide='prev'><span class='glyphicon glyphicon-chevron-left'>");
+	outerDiv.append("<a class='right carousel-control' href='#carousel_" + i + 
+	    	"' role='button' data-slide='next'><span class='glyphicon glyphicon-chevron-right'>");
+
+	return outerDiv;
+ }
+
+function addItemToCarousel(cNum, caption, url){
+
+	//console.log("Adding " + caption + "to carousel #" + cNum);
+
+	var id = "#carousel_" + cNum;
+	var listIndex = $(id).children(".carousel-indicators").children().length;
+	var active = "";
+	if(listIndex === 0){
+		active = "active";
+	}
+	$(id).children(".carousel-indicators").append("<li data-target='" + id +
+	            "' data-slide-to='"+listIndex+"' class='"+active+"'></li>");
+
+
+	var item = $("<div class='item "+active+"'></div>");
+    item.append("<img src='"+url+"' alt=''>");
+    var divCap = $("<div class='carousel-caption'></div>");
+    divCap.append("<h3>"+caption+"</h3>");
+    item.append(divCap);
+
+    $(id).children(".carousel-inner").append(item);
 }
